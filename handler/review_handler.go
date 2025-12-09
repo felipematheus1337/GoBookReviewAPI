2db021de333bb633733a79207b6eb14831586bb8
felipematheus1337 <lipehbr@gmail.com>
2025-12-09 12:18:03 -0300

    Finalizado review handler.

--- handler/review_handler.go
+++ handler/review_handler.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/felipematheus1337/GoBookReviewAPI/dto"
 	"github.com/felipematheus1337/GoBookReviewAPI/service"
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +16,32 @@ func NewReviewHandler(s service.ReviewService) *ReviewHandler {
 	return &ReviewHandler{Service: s}
 }
 
-func (h *ReviewHandler) Create(ctx *gin.Context) {}
+func (h *ReviewHandler) Create(ctx *gin.Context) {
+	var reviewDTO dto.ReviewDTO
 
-func (h *ReviewHandler) List(ctx *gin.Context) {}
+	if err := ctx.ShouldBind(&reviewDTO); err != nil {
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	response, err := h.Service.Create(reviewDTO)
+
+	if err != nil {
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "create-review", response, http.StatusOK)
+}
+
+func (h *ReviewHandler) List(ctx *gin.Context) {
+	reviews, err := h.Service.List()
+
+	if err != nil {
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "list-reviews", reviews, http.StatusOK)
+
+}
