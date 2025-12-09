ba98e2f916a8198f8f7d549d1ec563f5d5694476
felipematheus1337 <lipehbr@gmail.com>
2025-12-09 10:56:59 -0300

    Criado main

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/felipematheus1337/GoBookReviewAPI/config"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+
+	err := config.Init()
+
+	if err != nil {
+		fmt.Printf("Error initializing config: %v", err)
+	}
+
+	r := gin.Default()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = ":8080"
+	}
+
+	db, err := config.InitializeMySQL()
+
+	if err != nil {
+		fmt.Printf("Error initializing MySQL: %v", err)
+	}
+
+	_ = db
+
+	r.Run(port)
 
 }
