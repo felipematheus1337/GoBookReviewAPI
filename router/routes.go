3818d9004836b815e328d45dce4110c01e6458e6
felipematheus1337 <lipehbr@gmail.com>
2025-12-09 11:03:08 -0300

    Criado Routes.

---
+++ router/routes.go
@@ -0,0 +1,36 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/swag/example/override/docs"
+)
+
+func InitializeRoutes(router *gin.Engine, b *handler.BookHandler, r *handler.ReviewHandler) {
+
+	basePath := "api/v1/book"
+
+	docs.SwaggerInfo.BasePath = basePath
+
+	v1Books := router.Group(basePath)
+
+	v1Reviews := router.Group(basePath)
+
+	RegisterBookRoutes(v1Books, b)
+
+	RegisterReviewRoutes(v1Reviews, r)
+
+}
+
+func RegisterBookRoutes(v1 *gin.RouterGroup, b *handler.BookHandler) {
+	{
+		v1.POST("/", b.CreateBooks)
+		v1.GET("/", b.ListBooks)
+	}
+}
+
+func RegisterReviewRoutes(v1 *gin.RouterGroup, r *handler.ReviewHandler) {
+	{
+		v1.POST("/", r.CreateReview)
+		v1.GET("/", r.ListReview)
+	}
+}
